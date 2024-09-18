27913c3bbb38a58d059ede0dd80cbc2812ca4b13
dharun <dharundayaal@gmail.com>
2024-09-18 23:51:42 +0530

    three more elements shoud be added in the navbar

---
+++ src/component/NavItemComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../component/NavbarComponent.css"
+
+const NavItemComponent = ({name, svgElement}) => {
+  return (
+    <>
+        <div className="nav-item">
+            <div className="listOfNavItems">
+                <>{svgElement}</>
+                <h5>{name}</h5>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default NavItemComponent;
